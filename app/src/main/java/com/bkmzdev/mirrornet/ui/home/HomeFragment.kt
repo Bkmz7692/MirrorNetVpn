61be57fda4585432b378396052e67fbc64aef2bb
Ilya Korotaev <101642629+Bkmz7692@users.noreply.github.com>
2024-01-03 18:42:11 +0400

    122312

--- app/src/main/java/com/bkmzdev/mirrornet/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bkmzdev/mirrornet/ui/home/HomeFragment.kt
@@ -1,13 +1,26 @@
 package com.bkmzdev.mirrornet.ui.home
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Switch
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bkmzdev.mirrornet.databinding.FragmentHomeBinding
+import com.wireguard.android.backend.Backend
+import com.wireguard.android.backend.GoBackend
+import com.wireguard.android.backend.Tunnel
+import com.wireguard.android.backend.Tunnel.State
+import com.wireguard.config.Config
+import com.wireguard.config.InetEndpoint
+import com.wireguard.config.InetNetwork
+import com.wireguard.config.Interface
+import com.wireguard.config.Peer
+
 
 class HomeFragment : Fragment() {
 
@@ -17,26 +30,67 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    @Suppress("UNREACHABLE_CODE")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = "Это главная"
-        }
+
+
+
+
         return root
+        val switch: Switch = this.binding.switch3
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                println("Standard Switch is on")
+            }else{
+                println("Standard Switch is off")
+            }
+        }
+
+
+        val tunnel: Tunnel = WgTunnel()
+        val intentPrepare: Intent = GoBackend.VpnService.prepare(view?.context)
+        val interfaceBuilder = Interface.Builder()
+        val peerBuilder = Peer.Builder()
+        val backend: Backend = GoBackend(view?.context)
+        println("END OF INIT")
+        Log.d("bkTag", "Init Complite")
+        Log.d("bkTag", "Try To connect")
+        println("TRY TO CONNECT")
+        backend.setState(tunnel, State.UP, Config.Builder().setInterface(interfaceBuilder.addAddress(
+            InetNetwork.parse("10.0.3.200/32")).parsePrivateKey("redacted").build())
+            .addPeer(peerBuilder.addAllowedIp(InetNetwork.parse("0.0.0.0/0")).setEndpoint(
+                InetEndpoint.parse("46.101.188.154:51820")).parsePublicKey("redacted").build())
+            .build())
+
+
+
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    class WgTunnel : Tunnel {
+        override fun getName(): String {
+            return "wgpreconf"
+        }
+
+        override fun onStateChange(newState: State) {
+
+        }
     }
 }
